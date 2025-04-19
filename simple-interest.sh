80721beccbb58a3e787b7a6ef892e6ce981d586c
dantestivaktas <stivaktasd@gmail.com>
2025-04-19 14:49:38 -0500

    Initial commit with simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script calculates simple interest.
+
+echo "Enter the principal amount:"
+read principal
+
+echo "Enter the rate of interest (in %):"
+read rate
+
+echo "Enter the time period (in years):"
+read time
+
+# Simple Interest Formula
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "The simple interest is: $interest"
